3cad717a2f2590014a5c6e19b42c5cad7d1d1ee1
Kevin Mason <krmason2020@gmail.com>
2023-10-27 11:34:40 -0700

    .

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import App from './App';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const routing = (
+  <Router>
+    <React.StrictMode>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={App} />
+      </Switch>
+      <Footer />
+    </React.StrictMode>
+  </Router>
 );
+
+ReactDOM.render(routing, document.getElementById('root'));
